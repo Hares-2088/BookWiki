668939092b65f427960a6fbdfeaed4293f101cf7
PortgasD.Adem <121312399+Hares-2088@users.noreply.github.com>
2024-10-14 21:11:17 -0400

    first steps to add this project to docker

---
+++ books-service/src/main/java/com/bessam/bookwiki/dataaccesslayer/author/Author.java
@@ -0,0 +1,34 @@
+package com.bessam.bookwiki.dataaccesslayer.author;
+
+
+import com.bessam.bookwiki.dataaccesslayer.book.Book;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;// private id
+
+    @Column(name = "authorid")
+    private String authorId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "pictureURL")
+    private String pictureURL;
+
+    @OneToMany(mappedBy = "author")
+    private Set<Book> books;
+}
